ac47580fdd426a1c6e943d365d5739bfe830b83a
forthedamn <zhoguoxin@126.com>
2015-01-09 17:57:48 +0800

    发帖，看帖功能初步完成

--- forum/routes/index.js
+++ forum/routes/index.js
@@ -2,15 +2,28 @@ var express = require('express');
 var router = express.Router();
 var crypto = require('crypto');
 var User = require('../models/user.js');
+var Post = require('../models/post.js');
+var URL = require('url');
+var querystring = require('querystring');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Forum首页',
-    user: req.session.user
+  Post.get(null,function(err,posts){
+    if(err){
+      posts = []; 
+    };
+  res.render('index', { 
+      title: 'Forum首页',
+      user: req.session.user,
+      posts: posts,
+      success: req.flash('success').toString(),
+      error: req.flash('error').toString()
+      });
    });
 });
 
 /******注册*******/
+router.get('/reg',checkNotLogin);
 router.get('/reg',function(req,res){
 	res.render('reg', {
 		title: '注册',
@@ -57,6 +70,7 @@ router.post('/reg',function(req,res){
 });
 
 /******登录*******/
+router.get('/login',checkNotLogin);
 router.get('/login',function(req,res){
 	res.render('login', {title: '登录',
     error:req.flash('error').toString()
@@ -85,6 +99,7 @@ router.post('/login',function(req,res){
 });
 
 /******登出*******/
+router.get('/logout',checkLogin);
 router.get('/logout',function(req,res){
   req.session.user=null;
    req.flash('success','登出成功!');
@@ -92,7 +107,51 @@ router.get('/logout',function(req,res){
 });
 
 /******发帖*******/
+router.get('/write',checkLogin);
 router.get('/write',function(req,res){
   res.render('write',{title:'发帖'})
 });
+router.post('/write',function(req,res){
+  var currentUser = req.session.user,
+    post = new Post(currentUser.name,req.body.title,req.body.post);
+    post.save(function(err){
+      if(err){
+        req.flash('error',err);
+        return res.redirect('/');
+      }
+      req.flash('success','发布成功！');
+      res.redirect('/');
+    });
+});
+
+/**查看帖子**/
+router.get('/article',checkLogin);
+router.get('/article',function(req,res){
+  var arg = URL.parse(req.url).query;   //取出锚点a中href的值
+  var id = querystring.parse(arg).id;
+  Post.get(id,function(err,posts){
+    if(err){
+      posts = [];
+    };
+    res.render('article', {
+      title: 'forum',
+      posts: posts,
+    });
+  });
+ 
+});
+function checkNotLogin(req,res,next){
+  if(req.session.user){
+    req.flash('error','已登录！');
+    return res.redirect('/');
+  }
+  next();
+}
+function checkLogin(req,res,next){
+  if(!req.session.user){
+    req.flash('error','未登录，请先登录！');
+    return res.redirect('/login');
+  }
+  next();
+}
 module.exports = router;
